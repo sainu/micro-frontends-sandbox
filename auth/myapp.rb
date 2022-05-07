50d78a622765f6ebba49d0b5632d008f6dc5f3fd
sainu <sainu@users.noreply.github.com>
2022-05-08 03:59:06 +0900

    Create auth app

---
+++ auth/myapp.rb
@@ -0,0 +1,75 @@
+require 'sinatra'
+require 'sinatra/reloader' if settings.development?
+require 'rack/flash'
+require 'logger'
+
+set :sessions, domain: 'auth.lvh.me'
+use Rack::Flash
+
+logger = Logger.new(STDOUT)
+User = Struct.new('User', :id, :name, keyword_init: true)
+LoginForm = Struct.new('LoginForm', :name, :redirect_url, :error, keyword_init: true)
+
+def fetch_users
+  @users ||= [
+    User.new(id: 1, name: '佐藤 一'),
+    User.new(id: 2, name: '山田 太郎')
+  ]
+end
+
+before do
+  @current_user = fetch_users.find { |user| session[:user_id] == user.id }
+  @user_signed_in = !!@current_user
+end
+
+get '/health' do
+  'OK'
+end
+
+get '/' do
+  erb :index
+end
+
+get '/login' do
+  if @user_signed_in
+    flash[:notice] = 'すでにログインしています'
+    redirect '/'
+  end
+
+  @users = fetch_users
+  @form = LoginForm.new(redirect_url: params[:redirect_url])
+  erb :login
+end
+
+post '/login' do
+  if @user_signed_in
+    flash[:notice] = 'すでにログインしています'
+    redirect '/'
+  end
+
+  @form = LoginForm.new(name: params[:name], redirect_url: params[:redirect_url])
+
+  @users = fetch_users
+  user = @users.find { |u| u.name == @form.name }
+  unless user
+    @form.error = true
+    return erb :login
+  end
+
+  session[:user_id] = user.id
+
+  if @form&.redirect_url
+    # You must prevent open redirect attacks in the production environment
+    redirect @form.redirect_url
+  else
+    flash[:notice] = 'ログインしました'
+    redirect '/'
+  end
+end
+
+post '/logout' do
+  session[:user_id] = nil
+
+  flash[:notice] = 'ログアウトしました'
+  redirect '/'
+end
